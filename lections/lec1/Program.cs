3f9d5b29cbe7d60837fa04ba6c2cf7bf8d4887e9
Iren <kovalenkovaiv@gmail.com>
2022-10-02 17:41:08 +0400

    added ifElse

--- lections/lec1/Program.cs
+++ lections/lec1/Program.cs
@@ -1,6 +1,24 @@
-﻿int numberA = new Random().Next(1,10);
+﻿/*
+int numberA = new Random().Next(1,10);
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
+*/
+
+/*
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Даня")
+{ 
+Console.WriteLine("Ура, это же ДАНЯ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+*/
+

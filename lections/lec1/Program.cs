85cda107f6436698d76aa8c592acceb1ed37cb0e
Iren <kovalenkovaiv@gmail.com>
2022-10-02 20:46:32 +0400

    tree

--- lections/lec1/Program.cs
+++ lections/lec1/Program.cs
@@ -38,4 +38,19 @@ Console.Write("max = ");
 Console.WriteLine(max);
 */
 
+Console.Clear();
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 40;
+int yc = 30;
 
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");

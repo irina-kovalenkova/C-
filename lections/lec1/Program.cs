7fc0f82c321b1bae668340cc566f3b9c3d83a56e
Iren <kovalenkovaiv@gmail.com>
2022-10-02 21:26:26 +0400

    tree continue

--- lections/lec1/Program.cs
+++ lections/lec1/Program.cs
@@ -39,12 +39,10 @@ Console.WriteLine(max);
 */
 
 Console.Clear();
-int xa = 1;
-int ya = 1;
-int xb = 1;
-int yb = 30;
-int xc = 40;
-int yc = 30;
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -54,3 +52,28 @@ Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+while (count <10000)
+{
+int what = new Random().Next(0, 3);
+if (what == 0)
+{
+    x = (x + xa)/2;
+    y = (y + ya)/2;
+}
+if (what ==1)
+{
+    x = (x + xb)/2;
+    y = (y + yb)/2;
+}
+if (what == 2)
+{
+    x = (x + xc)/2;
+    y = (y + yc)/2;
+}
+Console.SetCursorPosition(x, y);
+Console.WriteLine("+");
+count ++;
+}

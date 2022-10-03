c4edc744f94d3381306c3c2ad14a6d1ab8bc61b6
Iren <kovalenkovaiv@gmail.com>
2022-10-03 17:21:19 +0400

    added second task

--- seminars/sem1/Program.cs
+++ seminars/sem1/Program.cs
@@ -17,3 +17,26 @@ max =b;
 Console.Write("max = ");
 Console.WriteLine(max);
 */
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+//Решение:
+/*
+Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+if(b>max)max = b;
+if(c>max)max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
+*/

59d7168e97a5d1e8cb8921d6c92b9e2f19702b15
Iren <kovalenkovaiv@gmail.com>
2022-10-03 17:46:45 +0400

    added third task

--- seminars/sem1/Program.cs
+++ seminars/sem1/Program.cs
@@ -40,3 +40,28 @@ if(c>max)max = c;
 Console.Write("max = ");
 Console.WriteLine(max);
 */
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*4 -> да
+-3 -> нет
+7 -> нет*/
+
+//Решение:
+/*
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = number%2;
+
+if(a==0)
+{
+    Console.WriteLine("да");
+    }
+else
+{
+Console.WriteLine("нет");
+}
+*/
+
+
+

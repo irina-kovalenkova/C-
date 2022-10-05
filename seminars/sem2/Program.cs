615ede6a2f86c8d8a6ad2d0e18b7f29eeec11c5d
Iren <kovalenkovaiv@gmail.com>
2022-10-05 22:48:41 +0400

    the third tasc

--- seminars/sem2/Program.cs
+++ seminars/sem2/Program.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿//Задача 1. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+/*
 int FindBiggestDigit(int num)
 
 {
@@ -18,6 +20,8 @@ int biggestDigit = FindBiggestDigit(number);
 Console.WriteLine($"The biggest digit of {number} is {biggestDigit}");
 */
 
+//Задача 2. Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+/*
 int TwoDigitNumber(int num)
 {
     int result;
@@ -29,3 +33,28 @@ int TwoDigitNumber(int num)
     int number = new Random().Next(100,1000);
     int twoDigitNumber = TwoDigitNumber(number);
     Console.WriteLine("Number is :" + twoDigitNumber);
+*/
+
+//Задача 3. Напишите программу, котрая принимает на вход число N и проверяет , кратно ли оно одновременно a и b (информация от пользователя.)
+
+bool MultipleNumber (int number, int a, int b)
+{
+    bool result;
+    if (number % a == 0 && number % b == 0)
+    {
+        result = true;
+    }
+    else
+    {
+        result = false;
+    }
+    return result;
+}
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the first division: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second division: ");
+int b = Convert.ToInt32(Console.ReadLine());
+bool res = MultipleNumber(number, a, b);
+Console.WriteLine(res);

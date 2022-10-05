4081672f3d6f4f9416405801fd83ce3faa227373
Iren <kovalenkovaiv@gmail.com>
2022-10-05 13:44:02 +0400

    task about biggest digit

---
+++ seminars/sem2/Program.cs
@@ -0,0 +1,17 @@
+﻿int FindBiggestDigit(int num)
+
+{
+    int max;
+    int ed = num % 10;
+    int dec = num / 10;
+
+    if(ed > dec)
+    max = ed;
+    else 
+    max = dec;
+    return max;
+}
+
+int number = new Random().Next(10,100);
+int biggestDigit = FindBiggestDigit(number);
+Console.WriteLine($"The biggest digit of {number} is {biggestDigit}");

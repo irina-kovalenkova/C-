a1398fc85dada80f3b04cc7ac9b02fc968da7636
Iren <kovalenkovaiv@gmail.com>
2022-10-05 21:54:47 +0400

    task about three digit

--- seminars/sem2/Program.cs
+++ seminars/sem2/Program.cs
@@ -1,4 +1,5 @@
-﻿int FindBiggestDigit(int num)
+﻿/*
+int FindBiggestDigit(int num)
 
 {
     int max;
@@ -15,3 +16,16 @@
 int number = new Random().Next(10,100);
 int biggestDigit = FindBiggestDigit(number);
 Console.WriteLine($"The biggest digit of {number} is {biggestDigit}");
+*/
+
+int TwoDigitNumber(int num)
+{
+    int result;
+    int ed = num % 10;
+    int dec = num / 100;
+    result = dec * 10 + ed;
+    return result;
+    }
+    int number = new Random().Next(100,1000);
+    int twoDigitNumber = TwoDigitNumber(number);
+    Console.WriteLine("Number is :" + twoDigitNumber);

d2a5d5be93a3d64e98edc4bf84ebf79003f2af68
Iren <kovalenkovaiv@gmail.com>
2022-10-07 12:33:16 +0400

    added first task

---
+++ DS/ds22/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+//Решение:
+/*
+int SecondDigitNumber(int num)
+{
+    int result;
+    int ed = num % 100;
+    int dec = ed / 10;
+    result = dec;
+    return result;
+}
+Console.Write("Enter a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigitNumber = SecondDigitNumber(number);
+Console.WriteLine("The number in the middle :" + secondDigitNumber);
+*/

aa7423d52fea9f7da4c171e33058bb09b682560e
Iren <kovalenkovaiv@gmail.com>
2022-10-12 21:50:03 +0400

    Home work 3

---
+++ DS/ds3/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+
+
+bool Polindrom (int num)
+
+
+
+
+
+
+/*
+Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+/*
+double Dictance(double xa, double xb, double ya, double yb, double zb, double za)
+{
+    return Math.Sqrt(Math.Pow((xb - xa),2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
+}
+Console.Write("Input xa - coordinate: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input xb - coordinate: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input ya - coordinate: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input yb - coordinate: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input za - coordinate: ");
+double za = Convert.ToDouble(Console.ReadLine());                    
+Console.Write("Input zb - coordinate: ");
+double zb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Dictance(xa, xb, ya, yb, za, zb));
+
+*/
+
+/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+// Решение:
+/*
+void WriteQ (int n)
+{
+    int count = 1;
+    while (count <= n)
+    {
+        Console.WriteLine(Math.Pow(count, 3));
+        count++;
+    }
+}
+Console.WriteLine("Input N number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+WriteQ(n);
+*/

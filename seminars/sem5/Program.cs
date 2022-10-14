ab75d795fe0cc010cd5f6f2404329dbc2484e138
Iren <kovalenkovaiv@gmail.com>
2022-10-14 20:43:22 +0400

    Seminar 5

--- seminars/sem5/Program.cs
+++ seminars/sem5/Program.cs
@@ -1,8 +1,45 @@
 ﻿//Задача 1. Задайте массив из 12 элементов, заполненный случайными числами из промежутка 
 //[-9,9]. Найдите сумму отрицательных и положительных элементов массива.
 
+int []CreateArray(int size)
+{
+    int[]array = new int [size];
+    Console.WriteLine("Create array: ");
 
+    for (int i = 0; i < size; i ++)
+    {
+        Console.Write($"Input a {i + 1} element of array: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        }
 
+    Console.WriteLine("/n Complete!");
+    return array;
+}
+
+void ShowArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i ++)
+    Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int SumOfNegatives(int[]array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i ++)
+    {
+        if (array[i] < 0)
+        sum += array[i];
+    }
+    return sum;
+}
+
+Console.Write("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(size);
+int result = SumOfNegatives(myArray);
+ShowArray(myArray);
+Console.WriteLine("Sum of a negative elements is" + result);
 
 
 

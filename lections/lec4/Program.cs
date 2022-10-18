7c264c24dd82fa2c8a98a39b9efde9419f760faf
Iren <kovalenkovaiv@gmail.com>
2022-10-18 16:53:19 +0400

    Lection 4

---
+++ lections/lec4/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+int[,]matrix = new int [3,4];
+for (int i = 0; i < 3; i ++)
+{
+    for (int j = 0; j < 4; j ++)
+    {
+        Console.Write($"{matrix[i, j]}");
+    }
+    Console.WriteLine();
+    }
+
+*/
+
+//Задача. Разукрасить руку.
+
+int [,]pic = new int[23, 25];
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i,j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+PrintImage(pic);
+
+
+
+
+
+    

51498cf79d43c231f3619b9a0488e2cb7bed31cb
Iren <kovalenkovaiv@gmail.com>
2022-10-21 17:25:26 +0400

    sem7

---
+++ seminars/sem7/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int[,] CreateRandom2dArray()
+
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int [rows, columns];
+    for (int i = 0; i < rows; i ++)
+    {
+        for (int j = 0; j < columns; j ++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+        return array;
+    }
+
+}
+
+void Show2dArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j ++)
+        Console.Write(array[i.j] + " ");
+        Console.WriteLine();
+    }
+}
+
+int[,]myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+
+
